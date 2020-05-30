00003afb604ac3ab73b739b60061d562d786062a
Crowdbotics <team@crowdbotics.com>
2020-05-30 01:22:15 +0000

    add package blueprint-react-native-articles-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,5 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
+{ name: 'ArticleList55062', human_name: 'Article List', access_route: 'ArticleList55062'},
 { name: 'ArticleList55061', human_name: 'Article List', access_route: 'ArticleList55061'},
 { name: 'ArticleList55060', human_name: 'Article List', access_route: 'ArticleList55060'},
 { name: 'Messenger', human_name: 'Messenger', access_route: 'Messenger', icon: 'comments'},
--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -5,6 +5,7 @@ import {createDrawerNavigator} from 'react-navigation-drawer';
 import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
+import ArticleList55062Navigator from '../features/ArticleList55062/navigator';
 import ArticleList55061Navigator from '../features/ArticleList55061/navigator';
 import ArticleList55060Navigator from '../features/ArticleList55060/navigator';
 import MessengerNavigator from '../features/Messenger/navigator';
@@ -23,6 +24,7 @@ const AppNavigator = {
       screen: SplashScreen
     },
     //@BlueprintNavigationInsertion
+ArticleList55062: { screen: ArticleList55062Navigator },
 ArticleList55061: { screen: ArticleList55061Navigator },
 ArticleList55060: { screen: ArticleList55060Navigator },
 Messenger: { screen: MessengerNavigator },
